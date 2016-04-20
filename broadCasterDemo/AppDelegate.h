166be06a46d306be374f9db659260e6b4a4c82d0
garin <garinspring@163.com>
2016-04-20 15:07:02 +0800

    Initial Commit

---
+++ broadCasterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  broadCasterDemo
+//
+//  Created by garin on 16/4/20.
+//  Copyright © 2016年 com.dangdang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ broadCasterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  broadCasterDemo
+//
+//  Created by garin on 16/4/20.
+//  Copyright © 2016年 com.dangdang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
